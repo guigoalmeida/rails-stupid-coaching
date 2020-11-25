b3e9b2bec3acec8df3e1b31d2a483acd98cb71da
Guilherme Almeida <guilherme.almeida@enap.gov.br>
2020-11-25 15:47:59 -0300

    funcionando mas poderia estar melhor formatado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  def ask; end
+
+  def answer
+    @user_input = params[:question]
+
+    @coach_answer = if @user_input == 'i am going to work'
+                      'Great!'
+                    elsif @user_input.end_with?('?')
+                      'Silly question, get dressed and go to work!'
+                    else
+                      " I don't care, get dressed and go to work!"
+                    end
+  end
 end
